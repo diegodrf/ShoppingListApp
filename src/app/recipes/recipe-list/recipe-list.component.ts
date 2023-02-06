ef8c16835f5371fc0b48e52f8de3ccd717019fa0
Diego Faria <diego.rdfaria@gmail.com>
2023-02-06 17:08:15 -0500

    ep 117

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,31 +1,26 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe";
+import {RecipeService} from "../../services/recipe.service";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() recipeChangeEvent = new EventEmitter<Recipe>();
   recipes: Recipe[];
 
-  constructor() {
-    this.recipes = [
-      new Recipe(
-        'A test Recipe',
-        'This is simply a test.',
-        'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-      ),
-      new Recipe(
-        'A test Recipe2',
-        'This is simply a test 2.',
-        'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
-      )
-    ];
+  constructor(private recipeService: RecipeService) {
+    this.recipes = [];
   }
+
 
   onSelectRecipe(recipe: Recipe) {
     this.recipeChangeEvent.emit(recipe);
+  }
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from "../recipes/recipe";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-
+  private readonly recipes = [
+    new Recipe(
+      'A test Recipe',
+      'This is simply a test.',
+      'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    ),
+    new Recipe(
+      'A test Recipe2',
+      'This is simply a test 2.',
+      'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+    )
+  ];
   constructor() { }
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
 }
